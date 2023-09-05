3df72d0af4c49941e578cc15ec215b99fa555ffc
matt curschman <mattcurschman@gmail.com>
2023-09-05 17:22:47 -0400

    first commit

---
+++ threads-clone/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <MainLayout>
+    <div id="IndexPage" class="w-full overflow-auto">
+        <div class="mx-auto max-w-[500px] overflow-hidden">
+          <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+
+            <div v-if="isPosts" v-for="post in posts" :key="post">
+              <Post :post="post" @isDeleted="posts = []"/>
+            </div>
+          </div>
+        </div>
+    </div>
+  </MainLayout>
+</template>
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue'
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+import {onBeforeMount} from "../.nuxt/imports";
+// const userStore = useUserStore()
+// const user = useSupabaseUser()
+
+let posts = ref([])
+let isPosts = ref(true)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+  posts.value = [
+    {
+      name: 'John',
+      image: 'https://placehold.co/100',
+      text: 'test',
+      picture: 'https://placehold.co/5100'
+    }
+  ]
+})
+</script>
